3509c796a6c4fa4164de2d11f13f99b601225e6a
anissen <anissen@cs.au.dk>
2010-06-02 08:34:17 +0000

    Small addition

--- Logic/DebugDecorator.cpp
+++ Logic/DebugDecorator.cpp
@@ -1,14 +1,15 @@
 // ?
 // -------------------------------------------------------------------
-// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
-// 
-// This program is free software; It is covered by the GNU General 
-// Public License version 2 or any later version. 
-// See the GNU General Public License for more details (see LICENSE). 
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS)
+//
+// This program is free software; It is covered by the GNU General
+// Public License version 2 or any later version.
+// See the GNU General Public License for more details (see LICENSE).
 //--------------------------------------------------------------------
 
 #include <Logic/DebugDecorator.h>
 #include <typeinfo>
+#include <stdio.h>
 #include <string>
 
 namespace OpenEngine {
@@ -19,7 +20,7 @@ namespace Logic {
 	}
 
 	DebugDecorator::~DebugDecorator() {
-	
+
 	}
 
 	Task::TaskStatus DebugDecorator::Execute() {
